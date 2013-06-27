17d658d5e5b44ee6062adb9f46da49b3cc98f0be
Jikoo <jikoo.games@gmail.com>
2013-06-26 23:09:53 -0400

    Keepalive method
    Thank you Tyler Blair knowing what the hell you're doing, I'll be
    reading and learning from your code :D

--- src/co/sblock/Sblock/DatabaseManager.java
+++ src/co/sblock/Sblock/DatabaseManager.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import co.sblock.Sblock.PlayerData.SblockPlayer;
 
 /**
