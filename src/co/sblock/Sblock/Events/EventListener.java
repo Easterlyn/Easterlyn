09fa17ae7250e32865e72a219090d81614b414ba
Jikoo <Jikoo.Games@gmail.com>
2014-02-07 04:54:16 -0500

    Use official 1.7.2 PacketWrapper update

--- src/co/sblock/Sblock/Events/EventListener.java
+++ src/co/sblock/Sblock/Events/EventListener.java
@@ -43,8 +43,8 @@ import co.sblock.Sblock.Chat.ChatUserManager;
 import co.sblock.Sblock.Chat.Channel.Channel;
 import co.sblock.Sblock.Chat.Channel.ChannelManager;
 import co.sblock.Sblock.Database.SblockData;
-import co.sblock.Sblock.Events.Packets.Packet11UseBed;
-import co.sblock.Sblock.Events.Packets.Packet12Animation;
+import co.sblock.Sblock.Events.Packets.WrapperPlayServerBed;
+import co.sblock.Sblock.Events.Packets.WrapperPlayServerAnimation;
 import co.sblock.Sblock.Events.Packets.SleepTeleport;
 import co.sblock.Sblock.SblockEffects.Cooldowns;
 import co.sblock.Sblock.UserData.Region;
@@ -290,7 +290,7 @@ public class EventListener extends PacketAdapter implements Listener {
 	private void fakeSleepDream(Player p, Location bed) {
 		ProtocolManager pm = ProtocolLibrary.getProtocolManager();
 
-		Packet11UseBed packet = new Packet11UseBed();
+		WrapperPlayServerBed packet = new WrapperPlayServerBed();
 		packet.setEntityId(p.getEntityId());
 		packet.setX(bed.getBlockX());
 		packet.setY((byte) bed.getBlockY());
@@ -320,9 +320,9 @@ public class EventListener extends PacketAdapter implements Listener {
 	 * @param p the Player
 	 */
 	public void fakeWakeUp(Player p) {
-		Packet12Animation packet = new Packet12Animation();
+		WrapperPlayServerAnimation packet = new WrapperPlayServerAnimation();
 		packet.setEntityID(p.getEntityId());
-		packet.setAnimation((byte) Packet12Animation.Animations.LEAVE_BED);
+		packet.setAnimation((byte) WrapperPlayServerAnimation.Animations.LEAVE_BED);
 
 		try {
 			ProtocolLibrary.getProtocolManager().sendServerPacket(p, packet.getHandle());
