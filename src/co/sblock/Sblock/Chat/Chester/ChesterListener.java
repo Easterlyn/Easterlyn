8401c2d1200016f7156e5309a0784e3920c5f43c
Jikoo <Jikoo.Games@gmail.com>
2014-04-04 04:45:00 -0400

    Minor tweaks Only compile Pattern once Do not increase cancels if a trigger is not occurring Update method of obtaining Log Remove command for resetting cancels - it is no longer needed.

--- src/co/sblock/Sblock/Chat/Chester/ChesterListener.java
+++ src/co/sblock/Sblock/Chat/Chester/ChesterListener.java
@@ -2,19 +2,18 @@ package co.sblock.Sblock.Chat.Chester;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import info.gomeow.chester.API.ChesterBroadcastEvent;
 import info.gomeow.chester.API.ChesterLogEvent;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import co.sblock.Sblock.CommandListener;
-import co.sblock.Sblock.SblockCommand;
 import co.sblock.Sblock.Chat.ChatUser;
 import co.sblock.Sblock.Chat.SblockChat;
 import co.sblock.Sblock.Chat.Channel.Channel;
@@ -28,13 +27,38 @@ public class ChesterListener implements CommandListener, Listener {
 
 	private int cancels = 0;
 
+	private Pattern pattern;
+
+	public ChesterListener() {
+		pattern = Pattern.compile(createRegex());
+		Log.getLog("ChesterListener").info("Compiled regex: " + pattern.toString());
+	}
+
+	private String createRegex() {
+		StringBuilder regex = new StringBuilder().append('(');
+		for (String s : SblockChat.chester) {
+			regex.append('(');
+			for (int i = 0; i < s.length(); i++) {
+				regex.append('[');
+				char ch = s.charAt(i);
+				if (Character.isLetter(ch)) {
+					regex.append(Character.toUpperCase(ch)).append(Character.toLowerCase(ch));
+				} else {
+					regex.append(ch);
+				}
+				regex.append(']');
+			}
+			regex.append(')').append('|');
+		}
+		regex.setCharAt(regex.length() - 1, ')');
+		return regex.toString();
+	}
+
 	@EventHandler
 	public void onChesterLog(ChesterLogEvent event) {
 		ChatUser c = ChatUser.getUser(event.getPlayer().getName());
 		if (c == null || c.getCurrent() == null) {
-			event.setCancelled(true);
-			cancels++;
-			new Log("ChesterListener", null).info("Not logging: No current channel. " + cancels + " trigger cancels.");
+			stopLogging(event);
 			return;
 		}
 
@@ -56,7 +80,7 @@ public class ChesterListener implements CommandListener, Listener {
 
 		for (String s : SblockChat.chester) {
 			if (event.getMessage().equalsIgnoreCase(s)) {
-				new Log("ChesterListener", null).info("Not logging: Chat equals trigger \"" + event.getMessage() + "\"");
+				Log.getLog("ChesterListener").info("Not logging: Chat equals trigger \"" + event.getMessage() + "\"");
 				event.setCancelled(true);
 				return;
 			}
@@ -64,30 +88,12 @@ public class ChesterListener implements CommandListener, Listener {
 	}
 
 	private void stopLogging(ChesterLogEvent event) {
-		StringBuilder regex = new StringBuilder().append('(');
-		for (String s : SblockChat.chester) {
-			regex.append('(').append(ignoreCaseRegex(s)).append(')').append('|');
-		}
-		regex.replace(regex.length() - 1, regex.length(), ")");
-		if (Pattern.compile(regex.toString()).matcher(event.getMessage()).find()) {
+		Matcher m = pattern.matcher(event.getMessage());
+		if (m.find()) {
 			cancels++;
+			Log.getLog("ChesterListener").info("Match found: " + m.group() + ". Cancels: " + cancels);
 		}
 		event.setCancelled(true);
-	}
-
-	private String ignoreCaseRegex(String s) {
-		StringBuilder regex = new StringBuilder();
-		for (int i = 0; i < s.length(); i++) {
-			regex.append('[');
-			char ch = s.charAt(i);
-			if (Character.isLetter(ch)) {
-				regex.append(Character.toUpperCase(ch)).append(Character.toLowerCase(ch));
-			} else {
-				regex.append(ch);
-			}
-			regex.append(']');
-		}
-		return regex.toString();
 	}
 
 	@EventHandler
@@ -95,7 +101,7 @@ public class ChesterListener implements CommandListener, Listener {
 		if (cancels > 0) {
 			event.getRecipients().clear();
 			cancels--;
-			new Log("ChesterListener", null).info("Chat cancelled when logged. " + cancels + " cancels remain.");
+			Log.getLog("ChesterListener").info("Chat cancelled when logged. " + cancels + " cancels remain.");
 			return;
 		}
 		Channel c = ChannelManager.getChannelManager().getChannel("#");
@@ -108,13 +114,5 @@ public class ChesterListener implements CommandListener, Listener {
 			listeners.add(Bukkit.getPlayerExact(s));
 		}
 		event.getRecipients().retainAll(listeners);
-	}
-
-	// For now, anyone can use. Not broadcasting its existence, but not gonna prevent them from fixing him.
-	@SblockCommand(description = "If Hal ain't talkin', here's how to come a-knockin'.", usage = "/fixchester")
-	public boolean fixchester(CommandSender s, String[] args) {
-		cancels = 0;
-		s.sendMessage("Resetting cancels.");
-		return true;
 	}
 }
