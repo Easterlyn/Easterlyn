84619006925ae2d18d77f434f2984fa7b4d1608b
Jikoo <Jikoo.Games@gmail.com>
2014-04-21 16:57:37 -0400

    Grist all the things!

--- src/co/sblock/Sblock/Utilities/Captcha/CruxiteDowel.java
+++ src/co/sblock/Sblock/Utilities/Captcha/CruxiteDowel.java
@@ -6,6 +6,7 @@ import java.util.Map.Entry;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
@@ -71,6 +72,20 @@ public class CruxiteDowel {
 			return cost;
 		}
 
+		for (Entry<Enchantment, Integer> entry : toCreate.getEnchantments().entrySet()) {
+			// (16 - weight) * level * 20 seems to be a good rate.
+			// Sharpness 5: 6 * 5 * 20 = 300 exp, 0-26
+			// silk touch 1: 15 * 1 * 20 = 300 exp
+			// fortune 3: 14 * 3 * 20 = 840 exp, 0-30
+			cost += (16 - getWeight(entry.getKey())) * entry.getValue() * 20;
+		}
+
+		if (toCreate.getItemMeta().hasDisplayName()) {
+			// Naming an unenchanted item in an anvil costs 5 levels.
+			// 0-5 is 85 exp.
+			cost += 85;
+		}
+
 		// if item contains special lore and !repairable, raise price
 		boolean willNeedRepair = toCreate.getItemMeta() instanceof Repairable;
 
@@ -90,6 +105,8 @@ public class CruxiteDowel {
 			}
 			cost += loreCost;
 		}
+
+		cost *= toCreate.getAmount();
 
 		return cost;
 	}
@@ -139,6 +156,7 @@ public class CruxiteDowel {
 			case HUGE_MUSHROOM_2:
 			case POTATO_ITEM:
 			case ROTTEN_FLESH:
+			case STONE:
 				materialValues.put(m.name(), 3);
 				break;
 			case ARROW:
@@ -155,7 +173,6 @@ public class CruxiteDowel {
 			case BAKED_POTATO:
 			case EGG:
 			case NETHER_BRICK:
-			case STONE:
 				materialValues.put(m.name(), 6);
 				break;
 			case COOKED_CHICKEN:
@@ -197,13 +214,11 @@ public class CruxiteDowel {
 			case SADDLE:
 				materialValues.put(m.name(), 16);
 				break;
-			case CHAINMAIL_BOOTS:
 			case SULPHUR:
 			case MAP: // Not crafted, right click
 			case MYCEL:
 				materialValues.put(m.name(), 20);
 				break;
-			case CHAINMAIL_HELMET:
 			case ENCHANTED_BOOK:
 				materialValues.put(m.name(), 25);
 				break;
@@ -214,16 +229,12 @@ public class CruxiteDowel {
 			case GRASS:
 				materialValues.put(m.name(), 30);
 				break;
-			case CHAINMAIL_LEGGINGS:
 			case GHAST_TEAR:
 				materialValues.put(m.name(), 35);
 				break;
 			case IRON_ORE:
 			case QUARTZ:
 				materialValues.put(m.name(), 37);
-				break;
-			case CHAINMAIL_CHESTPLATE:
-				materialValues.put(m.name(), 40);
 				break;
 			case IRON_INGOT:
 				materialValues.put(m.name(), 41);
@@ -278,13 +289,25 @@ public class CruxiteDowel {
 			case NAME_TAG:
 				materialValues.put(m.name(), 405);
 				break;
+			case CHAINMAIL_BOOTS:
+				materialValues.put(m.name(), 600);
+				break;
 			case GOLD_BARDING:
 				materialValues.put(m.name(), 663);
+				break;
+			case CHAINMAIL_HELMET:
+				materialValues.put(m.name(), 750);
 				break;
 			case DIAMOND_BARDING:
 			case DRAGON_EGG:
 			case NETHER_STAR:
 				materialValues.put(m.name(), 1000);
+				break;
+			case CHAINMAIL_LEGGINGS:
+				materialValues.put(m.name(), 1050);
+				break;
+			case CHAINMAIL_CHESTPLATE:
+				materialValues.put(m.name(), 1200);
 				break;
 			// UNOBTAINABLE
 			case AIR:
@@ -373,5 +396,23 @@ public class CruxiteDowel {
 			}
 		}
 		return minimum;
+	}
+
+	public static int getWeight(Enchantment e) {
+		if (e == Enchantment.PROTECTION_ENVIRONMENTAL || e == Enchantment.DAMAGE_ALL
+				|| e == Enchantment.DIG_SPEED || e == Enchantment.ARROW_DAMAGE) {
+			return 10;
+		}
+		if (e == Enchantment.WATER_WORKER || e == Enchantment.PROTECTION_EXPLOSIONS
+				|| e == Enchantment.OXYGEN || e == Enchantment.FIRE_ASPECT
+				|| e == Enchantment.LOOT_BONUS_MOBS || e == Enchantment.LOOT_BONUS_BLOCKS
+				|| e == Enchantment.ARROW_FIRE || e == Enchantment.ARROW_KNOCKBACK) {
+			return 2;
+		}
+		if (e == Enchantment.THORNS || e == Enchantment.SILK_TOUCH
+				|| e == Enchantment.ARROW_INFINITE) {
+			return 1;
+		}
+		return 5;
 	}
 }
