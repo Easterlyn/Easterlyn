b759f41736243f3f50e371cb3cb1ab02f3919275
Jikoo <Jikoo.Games@gmail.com>
2014-05-20 15:47:47 -0400

    Play nice with other chat plugins

--- src/co/sblock/events/listeners/PlayerAsyncChatListener.java
+++ src/co/sblock/events/listeners/PlayerAsyncChatListener.java
@@ -23,8 +23,21 @@ public class PlayerAsyncChatListener implements Listener {
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		event.setCancelled(true);
-		if (User.getUser(event.getPlayer().getUniqueId()) != null) {
-			User.getUser(event.getPlayer().getUniqueId()).chat(event.getMessage(), false);
+		// Clear recipients so as to not duplicate messages for global chat
+		event.getRecipients().clear();
+		User user = User.getUser(event.getPlayer().getUniqueId());
+		if (user != null) {
+			user.chat(event.getMessage(), false);
+			// Uncancel global chat to play nice with IRC plugins/Dynmap
+			if (user.isSuppressing() || user.isMute()) {
+				// Do not uncancel, user cannot chat in current state.
+			} else if (event.getMessage().startsWith("@# ") && event.getMessage().length() > 3) {
+				event.setMessage(event.getMessage().substring(3));
+				event.setCancelled(false);
+			} else if (user.getCurrent().getName().equals("#")
+					&& !(event.getMessage().startsWith("@") && event.getMessage().indexOf(' ') > 1)) {
+				event.setCancelled(false);
+			}
 		} else {
 			event.getPlayer().sendMessage(ChatColor.BOLD
 					+ "[o] Your Sblock playerdata appears to not be loaded."
