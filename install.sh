df0f5d1dee3bdb1f7b2a18d336c3fd6088f6f1e8
Ted Meyer <tmathmeyer@gmail.com>
2014-06-24 16:18:51 -0400

    added build script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+if [ -z "$*" ]; then
+	echo "Please supply the FULL path to sblock"
+else
+	ant -f build.xml
+	cp dist/Sblock.jar $1/plugins/
+	mkdir $1/plugins/lib
+	cp lib/j* $1/plugins/lib/
+	cp lib/gson* $1/plugins/lib/gson.jar
+fi
