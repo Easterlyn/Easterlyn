67a23a6bc7d2ba3d8fccd58d5c592af7c02835fc
Jikoo <Jikoo.Games@gmail.com>
2014-07-29 18:57:14 -0400

    Refreshes our db connection prior to login and prevents chat breaking.
    Hackish, yes. Fixable, yes. I don't have time now though.

--- src/co/sblock/data/sql/SQLClient.java
+++ src/co/sblock/data/sql/SQLClient.java
@@ -134,7 +134,7 @@ public class SQLClient extends SblockData {
 
 	@Override
 	public void getUserFromIP(String hostAddress, Promise<String> executor) {
-		executor.getObject(PlayerData.getUserFromIP(hostAddress), null);
+		PlayerData.getUserFromIP(hostAddress);
 	}
 
 	@Override
