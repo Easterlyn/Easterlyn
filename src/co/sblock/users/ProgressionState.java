91301ec31bad68c7bd3f2b0d277d5816eb04a813
Jikoo <Jikoo.Games@gmail.com>
2014-08-14 21:33:53 -0400

    Ordinals can be fickle, this is far safer for what I'd like to do.

--- src/co/sblock/users/ProgressionState.java
+++ src/co/sblock/users/ProgressionState.java
@@ -6,5 +6,29 @@ package co.sblock.users;
  * @author Jikoo
  */
 public enum ProgressionState {
-	NONE, ENTRY, GODTIER;
+	NONE(0), ENTRY_UNDERWAY(1), ENTRY_COMPLETING(2), ENTRY(3), GODTIER(10);
+
+	private final int value;
+	private ProgressionState(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * Gets the magic value representing this ProgressionState. Used to check if a User is beyond a
+	 * certain point in the game.
+	 * <p>
+	 * For example, checking if a User has achieved Entry or later in the game:
+	 * <pre>
+	 * user.getProgression().value() >= ProgressionState.ENTRY.value()
+	 * </pre>
+	 * <p>
+	 * This value may change but will remain constant relative to existing
+	 * ProgressionStates. It should not be used for saving or loading states, nor should it be used to
+	 * compare to fixed values.
+	 * 
+	 * @return
+	 */
+	public int value() {
+		return value;
+	}
 }
