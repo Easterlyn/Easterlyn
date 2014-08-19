3fe97710ea301f6a931750d53fb87f3c594b573b
Jikoo <Jikoo.Games@gmail.com>
2014-08-19 18:45:37 -0400

    Ping is not asynchronous and is causing lag spikes. Now it is async. Suck it, Bukkit.

--- src/co/sblock/events/listeners/PacketListener.java
+++ src/co/sblock/events/listeners/PacketListener.java
@@ -1,13 +1,25 @@
 package co.sblock.events.listeners;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedServerPing;
+
+import com.tmathmeyer.jadis.async.Promise;
 
 import co.sblock.Sblock;
 import co.sblock.chat.ColorDef;
 import co.sblock.chat.chester.ChesterListener;
+import co.sblock.data.SblockData;
 import co.sblock.events.SblockEvents;
+import co.sblock.events.packets.WrapperStatusServerOutServerInfo;
 
 /**
  * @author Jikoo
@@ -15,7 +27,8 @@ import co.sblock.events.SblockEvents;
 public class PacketListener extends PacketAdapter {
 
 	public PacketListener() {
-		super(Sblock.getInstance(), PacketType.Play.Client.ENTITY_ACTION, PacketType.Play.Client.CHAT);
+		super(Sblock.getInstance(), PacketType.Play.Client.ENTITY_ACTION, PacketType.Play.Client.CHAT,
+				PacketType.Status.Server.OUT_SERVER_INFO);
 	}
 
 	/**
@@ -45,5 +58,62 @@ public class PacketListener extends PacketAdapter {
 				}
 			}
 		}
+	}
+
+	/* (non-Javadoc)
+	 * @see com.comphenix.protocol.events.PacketAdapter#onPacketSending(com.comphenix.protocol.events.PacketEvent)
+	 */
+	@Override
+	public void onPacketSending(final PacketEvent event) {
+		if (event.getPacketType() != PacketType.Status.Server.OUT_SERVER_INFO) {
+			return;
+		}
+		final WrappedServerPing serverping = event.getPacket().getServerPings().read(0);
+		if (serverping.getVersionProtocol() == 9999) {
+			return;
+		}
+
+		// Causes client to see our custom message, cause woo! N.B. Does result in ping breaking and outdated client displaying.
+		serverping.setVersionProtocol(9999);
+
+		// Percent-based color: 0-49 = green, 50-74 = yellow, 75-100 = red
+		int percent = serverping.getPlayersOnline() * 100 / serverping.getPlayersMaximum();
+		ChatColor percentColor = percent > 75 ? ChatColor.RED : percent > 50 ? ChatColor.YELLOW : ChatColor.GREEN;
+
+		String versionName = ChatColor.GOLD + "Sblock Alpha 1.7.10 " + percentColor + serverping.getPlayersOnline()
+				+ ChatColor.DARK_GRAY + "/" + ChatColor.GREEN + serverping.getPlayersMaximum();
+		serverping.setVersionName(versionName);
+
+		event.setCancelled(true);
+
+		Bukkit.getScheduler().runTaskAsynchronously(getPlugin(), new Runnable() {
+			@Override
+			public void run() {
+				String addr = event.getPlayer().getAddress().getAddress().toString().substring(1);
+				SblockData.getDB().getUserFromIP(addr, getMOTDSettingExecutor(event, serverping));
+			}
+		});
+	}
+
+	private final Promise<String> getMOTDSettingExecutor(final PacketEvent event, final WrappedServerPing serverping) {
+		return new Promise<String>() {
+
+			@Override
+			public void getList(List<String> list) { }
+
+			@Override
+			public void getMap(Map<String, String> map) { }
+
+			@Override
+			public void getObject(String playerName, String unused) {
+				serverping.setMotD(Bukkit.getMotd().replaceAll("Player", playerName));
+				WrapperStatusServerOutServerInfo packet = new WrapperStatusServerOutServerInfo();
+				packet.setServerPing(serverping);
+				packet.sendPacket(event.getPlayer());
+			}
+
+			@Override
+			public void getSet(Set<String> set) { }
+		};
 	}
 }
