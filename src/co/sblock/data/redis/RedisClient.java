5f6ca8358424690110c1292f45c6bda0cb63cbc2
Ted Meyer <tmathmeyer@gmail.com>
2014-09-12 15:58:52 -0400

    new jadis, update to it's api see github.com/tmathmeyer/jadis for source

--- src/co/sblock/data/redis/RedisClient.java
+++ src/co/sblock/data/redis/RedisClient.java
@@ -107,17 +107,17 @@ public class RedisClient extends SblockData{
 
 	@Override
 	public void saveMachine(Machine m) {
-		connection.addSet("MACHINES", m.getSerialiser(), MachineSerialiser.class);
+		connection.addSet("MACHINES", m.getSerialiser(), exceptionLogger);
 	}
 
 	@Override
 	public void deleteMachine(Machine m) {
-		connection.remSet("MACHINES", exceptionLogger, m.getSerialiser());
+		connection.remFSet("MACHINES", m.getSerialiser(), exceptionLogger);
 	}
 
 	@Override
 	public void loadAllMachines() {
-		connection.getSet("MACHINES", machineDataPromise, MachineSerialiser.class, exceptionLogger);
+		connection.getASet("MACHINES", machineDataPromise, MachineSerialiser.class, exceptionLogger);
 	}
 
 	@Override
