eff0df64c51cc77fb812ed5778f33a89bcf58893
Jikoo <Jikoo.Games@gmail.com>
2014-11-06 01:11:36 -0500

    Fix dark aqua post-json injection and add cooldown to prevent cart bug

--- src/co/sblock/utilities/minecarts/FreeCart.java
+++ src/co/sblock/utilities/minecarts/FreeCart.java
@@ -1,6 +1,8 @@
 package co.sblock.utilities.minecarts;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.UUID;
 
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
@@ -18,12 +20,20 @@ public class FreeCart {
 	private static FreeCart instance;
 
 	private HashSet<Minecart> carts;
+	private HashMap<UUID, Long> cooldowns;
 
 	public FreeCart() {
 		carts = new HashSet<>();
+		cooldowns = new HashMap<>();
 	}
 
 	public void spawnCart(Player p, Location location, Vector startspeed) {
+		if (cooldowns.containsKey(p.getUniqueId())) {
+			if (cooldowns.get(p.getUniqueId()) >= System.currentTimeMillis())  {
+				return;
+			}
+		}
+		cooldowns.put(p.getUniqueId(), System.currentTimeMillis() + 200);
 		Minecart m = (Minecart) location.getWorld().spawnEntity(location, EntityType.MINECART);
 		m.setPassenger(p);
 		m.setVelocity(startspeed);
@@ -57,6 +67,7 @@ public class FreeCart {
 			cart.eject();
 			cart.remove();
 		}
+		cooldowns.clear();
 	}
 
 	public static FreeCart getInstance() {
