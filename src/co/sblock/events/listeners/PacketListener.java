0feb485c6650646adcec8a9e1ec0aa4222f271b7
Jikoo <Jikoo.Games@gmail.com>
2014-11-27 02:24:07 -0500

    Packet is being sent, not recieved.

--- src/co/sblock/events/listeners/PacketListener.java
+++ src/co/sblock/events/listeners/PacketListener.java
@@ -27,6 +27,38 @@ public class PacketListener extends PacketAdapter {
 	}
 
 	/**
+	 * 
+	 * @see com.comphenix.protocol.events.PacketAdapter#onPacketSending(com.comphenix.protocol.events.PacketEvent)
+	 * 
+	 * @param event the PacketEvent
+	 */
+	@Override
+	public void onPacketSending(PacketEvent event) {
+		if (event.getPacketType() == PacketType.Status.Server.OUT_SERVER_INFO) {
+			final WrappedServerPing serverping = event.getPacket().getServerPings().read(0);
+	
+			// Causes client to see our custom message, cause woo! N.B. Does result in ping breaking and outdated client displaying.
+			serverping.setVersionProtocol(9999);
+	
+			// Percent-based color: 0-49 = green, 50-74 = yellow, 75-100 = red
+			int percent = serverping.getPlayersOnline() * 100 / serverping.getPlayersMaximum();
+			ChatColor percentColor = percent > 75 ? ChatColor.RED : percent > 50 ? ChatColor.YELLOW : ChatColor.GREEN;
+	
+			String versionName = ChatColor.GOLD + "Sblock Alpha" + ChatColor.DARK_GRAY + ": "
+					+ ChatColor.GRAY + "1.7.10, 1.8" + ChatColor.DARK_GRAY + " - " + percentColor
+					+ serverping.getPlayersOnline() + ChatColor.DARK_GRAY + "/" + ChatColor.GREEN
+					+ serverping.getPlayersMaximum();
+			serverping.setVersionName(versionName);
+	
+			String addr = event.getPlayer().getAddress().getAddress().toString().substring(1);
+			String playerName = SblockEvents.getEvents().getIPName(addr);
+			SblockEvents.getEvents().getLogger().info(playerName + " pinged the server from " + addr);
+			serverping.setMotD(Bukkit.getMotd().replaceAll("Player", playerName));
+			event.getPacket().getServerPings().write(0, serverping);
+		}
+	}
+
+	/**
 	 * Check a packet from the client.
 	 * 
 	 * @see com.comphenix.protocol.events.PacketAdapter#onPacketReceiving(PacketEvent)
@@ -61,28 +93,6 @@ public class PacketListener extends PacketAdapter {
 			if (plugin.matcher(event.getPacket().getStrings().read(0)).find()) {
 				event.setCancelled(true);
 			}
-		}
-		if (event.getPacketType() == PacketType.Status.Server.OUT_SERVER_INFO) {
-			final WrappedServerPing serverping = event.getPacket().getServerPings().read(0);
-	
-			// Causes client to see our custom message, cause woo! N.B. Does result in ping breaking and outdated client displaying.
-			serverping.setVersionProtocol(9999);
-	
-			// Percent-based color: 0-49 = green, 50-74 = yellow, 75-100 = red
-			int percent = serverping.getPlayersOnline() * 100 / serverping.getPlayersMaximum();
-			ChatColor percentColor = percent > 75 ? ChatColor.RED : percent > 50 ? ChatColor.YELLOW : ChatColor.GREEN;
-	
-			String versionName = ChatColor.GOLD + "Sblock Alpha" + ChatColor.DARK_GRAY + ": "
-					+ ChatColor.GRAY + "1.7.10, 1.8" + ChatColor.DARK_GRAY + " - " + percentColor
-					+ serverping.getPlayersOnline() + ChatColor.DARK_GRAY + "/" + ChatColor.GREEN
-					+ serverping.getPlayersMaximum();
-			serverping.setVersionName(versionName);
-	
-			String addr = event.getPlayer().getAddress().getAddress().toString().substring(1);
-			String playerName = SblockEvents.getEvents().getIPName(addr);
-			SblockEvents.getEvents().getLogger().info(playerName + " pinged the server from " + addr);
-			serverping.setMotD(Bukkit.getMotd().replaceAll("Player", playerName));
-			event.getPacket().getServerPings().write(0, serverping);
 		}
 	}
 }
