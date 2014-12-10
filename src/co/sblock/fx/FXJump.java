56fc2b1722025a667a1c695b1ed1d6dd5aa2d9c3
Jikoo <Jikoo.Games@gmail.com>
2014-12-10 17:04:20 -0500

    Effect-a-week continues. I want my jump boost back.

---
+++ src/co/sblock/fx/FXJump.java
@@ -0,0 +1,37 @@
+package co.sblock.fx;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import co.sblock.users.OnlineUser;
+
+/**
+ * Jump boost passive effect
+ * 
+ * @author Jikoo
+ */
+public class FXJump extends SblockFX {
+
+	@SuppressWarnings("unchecked")
+	public FXJump() {
+		super("JUMP", true, 500, 0, PlayerPickupItemEvent.class, PlayerDropItemEvent.class,
+				InventoryCloseEvent.class, PlayerDeathEvent.class);
+		this.addCommonName("Boing");
+	}
+
+	@Override
+	protected void getEffect(OnlineUser u, Class<? extends Event> e) {
+		PotionEffect potEffect = new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, multiplier);
+		u.getPlayer().addPotionEffect(potEffect, true);
+	}
+
+	@Override
+	public void removeEffect(OnlineUser u) {
+		u.getPlayer().removePotionEffect(PotionEffectType.JUMP);
+	}
+}
