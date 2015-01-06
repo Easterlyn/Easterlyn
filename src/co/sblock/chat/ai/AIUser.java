c0c22a51fd7d8f21178ff399fe4e39a481b396fe
Jikoo <Jikoo.Games@gmail.com>
2015-01-06 16:27:35 -0500

    Hal just wants to be a real boy.

---
+++ src/co/sblock/chat/ai/AIUser.java
@@ -0,0 +1,32 @@
+package co.sblock.chat.ai;
+
+import java.util.HashSet;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import co.sblock.users.OfflineUser;
+import co.sblock.users.ProgressionState;
+import co.sblock.users.Region;
+import co.sblock.users.UserAspect;
+import co.sblock.users.UserClass;
+
+/**
+ * Extension of OfflineUser to allow fake player objects to be created.
+ * 
+ * @author Jikoo
+ */
+public class AIUser extends OfflineUser {
+
+	/**
+	 * @param displayName
+	 * @param userClass
+	 * @param userAspect
+	 * @param medium
+	 * @param dream
+	 */
+	protected AIUser(String displayName, UserClass userClass, UserAspect userAspect, Region medium, Region dream) {
+		super(UUID.randomUUID(), "localhost", displayName, null, null, "N/A", null,
+				userClass, userAspect, medium, dream, ProgressionState.NONE, null, null, new HashSet<Integer>(), false,
+				"#", new HashSet<String>(), new AtomicBoolean(false), new AtomicBoolean(false), new AtomicBoolean(false));
+	}
+}
