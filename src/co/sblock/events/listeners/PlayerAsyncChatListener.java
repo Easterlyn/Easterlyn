c0dbc2db40ad041ff5f426eea02d2adcb357c1e2
Jikoo <Jikoo.Games@gmail.com>
2015-01-09 07:03:17 -0500

    Better chat compatibility with other plugins Bonus "feature," automatically eats super fast spam.

--- src/co/sblock/events/listeners/PlayerAsyncChatListener.java
+++ src/co/sblock/events/listeners/PlayerAsyncChatListener.java
@@ -1,6 +1,8 @@
 package co.sblock.events.listeners;
 
 import java.util.HashSet;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -25,7 +27,7 @@ import co.sblock.users.Users;
  */
 public class PlayerAsyncChatListener implements Listener {
 
-	private String[] tests = new String[] {"It is certain.", "It is decidedly so.",
+	private final String[] tests = new String[] {"It is certain.", "It is decidedly so.",
 			"Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see, yes.",
 			"Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
 			"Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
@@ -34,15 +36,10 @@ public class PlayerAsyncChatListener implements Listener {
 			"Testing complete. Proceeding with operation.", "A critical fault has been discovered while testing.",
 			"Error: Test results contaminated.", "tset", "PONG."};
 
-	/**
-	 * The event handler for AsyncPlayerChatEvents.
-	 * 
-	 * We listen at monitor because we cancel the actual chat and send JSON messages instead.
-	 * 
-	 * @param event the AsyncPlayerChatEvent
-	 */
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-	public void onPlayerChat(final AsyncPlayerChatEvent event) {
+	private final ConcurrentHashMap<UUID, Message> messages = new ConcurrentHashMap<>();
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onPlayerChatLow(final AsyncPlayerChatEvent event) {
 		MessageBuilder mb = new MessageBuilder().setSender(Users.getGuaranteedUser(event.getPlayer().getUniqueId()));
 		mb.setMessage(event.getMessage());
 		// Ensure message can be sent
@@ -59,6 +56,7 @@ public class PlayerAsyncChatListener implements Listener {
 		}
 		if (message.getCleanedMessage().equalsIgnoreCase("hal") || message.getCleanedMessage().equalsIgnoreCase("dirk")) {
 			event.getPlayer().sendMessage(ColorDef.HAL + "What?");
+			event.setCancelled(true);
 			return;
 		}
 		// Because we have a collection of UUIDs and not Players, nice compact lambda notation instead of a loop
@@ -66,6 +64,29 @@ public class PlayerAsyncChatListener implements Listener {
 
 		event.setMessage(message.getMessage());
 		event.setFormat(message.getConsoleFormat());
+
+		messages.put(event.getPlayer().getUniqueId(), message);
+	}
+
+	/**
+	 * The event handler for AsyncPlayerChatEvents.
+	 * 
+	 * We listen at monitor because we cancel the actual chat and send JSON messages instead.
+	 * 
+	 * @param event the AsyncPlayerChatEvent
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerChat(final AsyncPlayerChatEvent event) {
+		final Message message;
+		if (messages.containsKey(event.getPlayer().getUniqueId())) {
+			message = messages.remove(event.getPlayer().getUniqueId());
+		} else {
+			message = null;
+			event.setCancelled(true);
+		}
+		if (event.isCancelled()) {
+			return;
+		}
 
 		final HashSet<Player> recipients = new HashSet<>(event.getRecipients());
 
