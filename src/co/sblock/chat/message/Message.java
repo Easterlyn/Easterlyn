1f4a0e2c126b8ac9d0d3dfcddb686bc16c513125
Jikoo <Jikoo.Games@gmail.com>
2015-01-11 00:30:03 -0500

    Oops. Gotta remove this.

--- src/co/sblock/chat/message/Message.java
+++ src/co/sblock/chat/message/Message.java
@@ -66,7 +66,7 @@ public class Message {
 			this.nick = null;
 		}
 
-		this.unformattedMessage = message.replace("\\", "\\\\").replace("\"", "\\\"");
+		this.unformattedMessage = message;
 		this.channelHighlightElement = getChannelPrefix(true);
 		this.nameElement = getSenderElement();
 	}
