9bab219d332eab985a3b360c9122e835848951d0
Jikoo <Jikoo.Games@gmail.com>
2015-02-06 13:48:49 -0500

    So that was spammy while I was set in the correct state to use it

--- src/co/sblock/effects/fx/FXGodtierHeartActive.java
+++ src/co/sblock/effects/fx/FXGodtierHeartActive.java
@@ -17,19 +17,17 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import co.sblock.Sblock;
 import co.sblock.users.OnlineUser;
 
 public class FXGodtierHeartActive extends SblockFX {
 
 	@SuppressWarnings("unchecked")
 	public FXGodtierHeartActive() {
-		super("FXGodtierHeartActive", false, 0, 1000 * 60, PlayerInteractEvent.class,PlayerInteractEntityEvent.class);
+		super("FXGodtierHeartActive", false, 0, 1000 * 60, PlayerInteractEvent.class, PlayerInteractEntityEvent.class);
 	}
 
 	@Override
 	protected void getEffect(OnlineUser user, Event event) {
-		Sblock.getLog().info("Applying Godtier Active Effect");
 		ArrayList<Entity> nearbyEntities = (ArrayList<Entity>) user.getPlayer().getNearbyEntities(8, 8, 8);
 		PotionEffect potEffect = new PotionEffect(PotionEffectType.REGENERATION, 20 * 20, 1);
 		user.getPlayer().addPotionEffect(potEffect, true);
@@ -37,31 +35,25 @@ public class FXGodtierHeartActive extends SblockFX {
 		loc.setY(loc.getY() + 1);
 		user.getPlayer().getWorld().playEffect(user.getPlayer().getLocation(), Effect.HEART, 0);
 		user.getPlayer().getWorld().playEffect(loc, Effect.HEART, 0);
-		for(Entity e : nearbyEntities) {
-			Sblock.getLog().info(e.getType() + "");
-			if(e instanceof Animals) {
+		for (Entity e : nearbyEntities) {
+			if (e instanceof Animals) {
 				Animals animal = (Animals) e;
-				if(animal.isAdult()) {
+				if (animal.isAdult()) {
 					loc = animal.getLocation();
 					loc.setY(loc.getY() + 1);
 					animal.setBreed(true);
-					
+
 					NBTTagCompound tag = new NBTTagCompound();
-					((CraftAnimals)animal).getHandle().b(tag);
+					((CraftAnimals) animal).getHandle().b(tag);
 					tag.setInt("InLove", 600);
-					((CraftAnimals)animal).getHandle().a(tag);
-					
+					((CraftAnimals) animal).getHandle().a(tag);
+
 					animal.getWorld().playEffect(animal.getLocation(), Effect.HEART, 0);
 					animal.getWorld().playEffect(loc, Effect.HEART, 0);
-					Sblock.getLog().info("Breeding");
-					Sblock.getLog().info(animal.getAge() + "");
-				}
-				else {
+				} else {
 					animal.setAge(animal.getAge() + 20 * 60 * 5);
-					Sblock.getLog().info(animal.getAge() + "");
 				}
-			}
-			else if(e.getType() == EntityType.PLAYER) {
+			} else if (e.getType() == EntityType.PLAYER) {
 				((Player) e).addPotionEffect(potEffect, true);
 				loc = ((Player) e).getLocation();
 				loc.setY(loc.getY() + 1);
@@ -72,7 +64,5 @@ public class FXGodtierHeartActive extends SblockFX {
 	}
 
 	@Override
-	public void removeEffect(OnlineUser user) {
-	}
-
+	public void removeEffect(OnlineUser user) {}
 }
