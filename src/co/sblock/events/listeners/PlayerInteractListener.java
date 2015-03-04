444bf261e71f9947f4de44a4d909b3ad213bd70e
Jikoo <Jikoo.Games@gmail.com>
2015-03-04 01:44:35 -0500

    Well, until I isolate the stuff I do want to ignore cancellation...

--- src/co/sblock/events/listeners/PlayerInteractListener.java
+++ src/co/sblock/events/listeners/PlayerInteractListener.java
@@ -9,7 +9,6 @@ import org.bukkit.Material;
 import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -47,7 +46,6 @@ public class PlayerInteractListener implements Listener {
 	 * @param event the PlayerInteractEvent
 	 */
 	@SuppressWarnings("deprecation")
-	@EventHandler(ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		OfflineUser user = Users.getGuaranteedUser(event.getPlayer().getUniqueId());
 		if (user instanceof OnlineUser && ((OnlineUser) user).isServer()) {
