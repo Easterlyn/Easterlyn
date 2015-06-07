9fa99bd86a6f289ccef2ae5e61c750c8643c091b
Jikoo <Jikoo.Games@gmail.com>
2015-06-07 13:29:19 -0400

    test

---
+++ src/co/sblock/commands/chat/TestCommand.java
@@ -0,0 +1,29 @@
+package co.sblock.commands.chat;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import co.sblock.commands.SblockCommand;
+
+/**
+ * DAMN IT ALL, BASEMENTHERO, FINE.
+ * 
+ * @author Jikoo
+ */
+public class TestCommand extends SblockCommand {
+
+	public TestCommand() {
+		super("test");
+	}
+
+	@Override
+	protected boolean onCommand(CommandSender sender, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("TEST FAILED, JERK.");
+			return true;
+		}
+		((Player) sender).chat("test");
+		return true;
+	}
+
+}
