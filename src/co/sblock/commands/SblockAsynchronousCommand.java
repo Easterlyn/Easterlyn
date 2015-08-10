de6741f75d11ecf486e59a5321f14cbd97efa1bc
Jikoo <Jikoo.Games@gmail.com>
2015-08-10 10:26:30 -0400

    Duplicate code sucks, so let's try this instead.

--- src/co/sblock/commands/SblockAsynchronousCommand.java
+++ src/co/sblock/commands/SblockAsynchronousCommand.java
@@ -6,8 +6,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-import org.apache.commons.lang3.StringUtils;
-
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -15,8 +13,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import co.sblock.Sblock;
-import co.sblock.chat.Color;
-import co.sblock.utilities.messages.Slack;
 
 /**
  * Base for commands that should be processed off the main thread.
@@ -31,33 +27,10 @@ public abstract class SblockAsynchronousCommand extends SblockCommand {
 
 	@Override
 	public boolean execute(CommandSender sender, String label, String[] args) {
-		if (getPermission() != null && !sender.hasPermission(getPermission())) {
-			sender.sendMessage(getPermissionMessage());
-			return true;
-		}
 		new BukkitRunnable() {
 			@Override
 			public void run() {
-				try {
-					if (!onCommand(sender, label, args)) {
-						sender.sendMessage(getUsage());
-					}
-				} catch (Exception e) {
-					sender.sendMessage(Color.BAD + "An error occurred processing this command. Please make sure your parameters are correct.");
-					StringBuilder cause = new StringBuilder("\nTrace base: ");
-					if (e.getMessage() != null) {
-						cause.append(e.getMessage());
-					} else {
-						cause.append("null");
-					}
-					if (e.getCause() != null && e.getCause().getMessage() != null) {
-						cause.append(", cause by: ").append(e.getCause().getMessage());
-					}
-					Slack.getInstance().postReport(sender.getName(), null,
-							"Error processing command: " + StringUtils.join(args, ' ')
-									+ cause.toString());
-					e.printStackTrace();
-				}
+				SblockAsynchronousCommand.super.execute(sender, label, args);
 			}
 		}.runTaskAsynchronously(Sblock.getInstance());
 		return true;
