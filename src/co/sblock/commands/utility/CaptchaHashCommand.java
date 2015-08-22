added2f66e88179fbaa95e4cee6afdd3453678f0
Jikoo <Jikoo.Games@gmail.com>
2015-08-22 15:51:22 -0400

    Allow Denizen+ to create custom hashes

---
+++ src/co/sblock/commands/utility/CaptchaHashCommand.java
@@ -0,0 +1,71 @@
+package co.sblock.commands.utility;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import co.sblock.captcha.Captcha;
+import co.sblock.chat.Color;
+import co.sblock.commands.SblockCommand;
+
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * SblockCommand for creating a new unique hash for an item or getting a captcha for a hash.
+ * 
+ * @author Jikoo
+ */
+public class CaptchaHashCommand extends SblockCommand {
+
+	public CaptchaHashCommand() {
+		super("hash");
+		this.setDescription("Create a new hash for your item in hand, or get a captcha for a specific hash.");
+		this.setUsage("/hash get <hash> | With item in hand, /hash add <hash>");
+		this.setPermissionLevel("denizen");
+		this.setPermissionMessage(ChatColor.GOLD + "BROWNS!");
+	}
+
+	@Override
+	protected boolean onCommand(CommandSender sender, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Console support not offered at this time.");
+			return true;
+		}
+		if (args.length < 2) {
+			return false;
+		}
+		if (!args[1].matches("[0-9A-Za-z]{8,}")) {
+			sender.sendMessage(Color.BAD + "Hashes must be 8 or more characters containing only 0-9, A-Z, a-z.");
+			return true;
+		}
+		args[0] = args[0].toLowerCase();
+		Player player = (Player) sender;
+		if (args[0].equals("add")) {
+			ItemStack item = player.getItemInHand();
+			if (item == null) {
+				return false;
+			}
+			if (Captcha.getInstance().saveItemStack(args[1], item)) {
+				sender.sendMessage(Color.GOOD + "Saved to " + args[1]);
+				return true;
+			} else {
+				sender.sendMessage(Color.BAD + "Hash " + args[1]
+						+ " is already in use. Manually delete the file if you're sure there will be no conflict.");
+				return true;
+			}
+		}
+		if (args[0].equals("get")) {
+			ItemStack item = Captcha.getInstance().getItemStack(args[1]);
+			if (item == null) {
+				sender.sendMessage(Color.BAD + "No item is stored by that hash.");
+				return true;
+			}
+			item = Captcha.itemToCaptcha(item);
+			player.getWorld().dropItem(player.getLocation(), item).setPickupDelay(0);
+			player.sendMessage(Color.GOOD + "Loaded captcha of item for " + args[1]);
+			return true;
+		}
+		return false;
+	}
+
+}
