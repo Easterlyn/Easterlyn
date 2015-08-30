448811c5a5c8de759eb5a8e40f0e87980c30c6a8
Jikoo <Jikoo.Games@gmail.com>
2015-08-30 18:53:58 -0400

    Convenience for Saros.

---
+++ src/co/sblock/commands/utility/FullRepairCommand.java
@@ -0,0 +1,59 @@
+package co.sblock.commands.utility;
+
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.Repairable;
+
+import com.google.common.collect.ImmutableList;
+
+import co.sblock.chat.Color;
+import co.sblock.commands.SblockCommand;
+
+/**
+ * SblockCommand for repairing an item
+ * 
+ * @author Jikoo
+ */
+public class FullRepairCommand extends SblockCommand {
+
+	public FullRepairCommand() {
+		super("fullrepair");
+		this.setDescription("Fully repairs an item, including wiping the anvil tag.");
+		this.setUsage("Run /fullrepair while holding an item.");
+		this.setPermissionLevel("denizen");
+	}
+
+	@Override
+	protected boolean onCommand(CommandSender sender, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Console support not offered at this time.");
+			return true;
+		}
+		Player player = (Player) sender;
+		ItemStack hand = player.getItemInHand();
+		if (hand == null) {
+			return false;
+		}
+		if (hand.getType().getMaxDurability() > 0) {
+			hand.setDurability((short) 0);
+		}
+		if (hand.hasItemMeta()) {
+			ItemMeta meta = hand.getItemMeta();
+			Repairable repairable = (Repairable) meta;
+			repairable.setRepairCost(0);
+			hand.setItemMeta(meta);
+		}
+		player.setItemInHand(hand);
+		player.sendMessage(Color.GOOD + "Fully repaired!");
+		return true;
+	}
+
+	@Override
+	public List<String> tabComplete(CommandSender sender, String alias, String[] args) {
+		return ImmutableList.of();
+	}
+}
