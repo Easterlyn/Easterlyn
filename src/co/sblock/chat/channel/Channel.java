9e8edb8347f203af747051500dd155a50f3d4607
Jikoo <Jikoo.Games@gmail.com>
2015-10-08 09:03:38 -0400

    Comparing to last message is problematic Oftentimes only greetings are muted - there are a LOT of false positives, and little actual spam is stopped.

--- src/co/sblock/chat/channel/Channel.java
+++ src/co/sblock/chat/channel/Channel.java
@@ -24,8 +24,6 @@ public abstract class Channel {
 	protected final Set<UUID> listening;
 	protected final UUID owner;
 
-	private String lastMessage;
-
 	/**
 	 * @param name the name of the channel
 	 * @param creator the owner of the channel
@@ -102,24 +100,6 @@ public abstract class Channel {
 	 * Update the last access time.
 	 */
 	public abstract void updateLastAccess();
-
-	/**
-	 * Gets the last player chat message sent through the channel.
-	 * 
-	 * @return the message sent
-	 */
-	public String getLastMessage() {
-		return lastMessage != null ? lastMessage : new String();
-	}
-
-	/**
-	 * Sets the last player chat message sent through the channel.
-	 * 
-	 * @param message the message
-	 */
-	public void setLastMessage(String message) {
-		lastMessage = message;
-	}
 
 	/**
 	 * For sending a channel message, not for chat! Chat should be sent by constructing a Message with a MessageBuilder.
