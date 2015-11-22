db806b04f2d4701abae0ba67a2a4e34fc82abdf3
Jikoo <Jikoo.Games@gmail.com>
2015-11-22 12:35:42 -0500

    Cache SpamHaus results for IPs for an hour to reduce requests

--- src/co/sblock/events/listeners/player/AsyncPreLoginListener.java
+++ src/co/sblock/events/listeners/player/AsyncPreLoginListener.java
@@ -1,7 +1,9 @@
 package co.sblock.events.listeners.player;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -14,6 +16,9 @@ import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
 import co.sblock.discord.Discord;
 import co.sblock.events.Events;
 import co.sblock.utilities.PermissionBridge;
@@ -24,6 +29,8 @@ import co.sblock.utilities.PermissionBridge;
  * @author Jikoo
  */
 public class AsyncPreLoginListener implements Listener {
+
+	private final Cache<String, Boolean> ipCache;
 
 	public AsyncPreLoginListener() {
 		Permission permission;
@@ -36,38 +43,63 @@ public class AsyncPreLoginListener implements Listener {
 		}
 		permission.addParent("sblock.command.*", true).recalculatePermissibles();
 		permission.addParent("sblock.helper", true).recalculatePermissibles();
+
+		ipCache = CacheBuilder.newBuilder().expireAfterWrite(1L, TimeUnit.HOURS).weakKeys()
+				.weakValues().build();
 	}
 
 	@EventHandler
 	public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
-		if (PermissionBridge.getInstance().hasPermission(event.getUniqueId(), "sblock.login.proxy")) {
-			return;
+		final String ip = event.getAddress().getHostAddress();
+		Boolean allowed = ipCache.getIfPresent(ip);
+		if (allowed == null) {
+			allowed = isAllowed(ip);
+			ipCache.put(ip, allowed);
 		}
-		try {
-			final String ip = event.getAddress().getHostAddress();
-			final String[] split = ip.split("\\.");
-			final StringBuilder lookup = new StringBuilder();
-			for (int i = split.length - 1; i >= 0; --i) {
-				lookup.append(split[i]);
-				lookup.append(".");
+		if (!allowed) {
+			allowed =  PermissionBridge.getInstance().hasPermission(event.getUniqueId(), "sblock.login.proxy");
+			if (allowed) {
+				// Players with permission can allow a blocked IP for 30 minutes by logging in from it
+				ipCache.put(ip, allowed);
 			}
-			if (!ip.contains("127.0.0.1")) {
-				lookup.append("xbl.spamhaus.org.");
-				if (InetAddress.getByName(lookup.toString()) != null) {
-					event.setLoginResult(Result.KICK_OTHER);
-					event.setKickMessage("Your IP address is flagged as unsafe by the Spamhaus XBL."
-							+"\n\nPlease visit https://www.spamhaus.org/query/ip/" + ip + " to learn why.");
-					Discord.getInstance().postReport(event.getUniqueId().toString(),
-							ip + " is flagged as unsafe by spamhaus.org/xbl, disconnecting " + event.getUniqueId());
-					return;
-				}
-			}
-		} catch (Exception ex) {}
+		}
 		Collection<String> ips = Events.getInstance().getIPsFor(event.getUniqueId());
 		if (ips.size() > 1) {
 			OfflinePlayer offline = Bukkit.getOfflinePlayer(event.getUniqueId());
 			String name = offline.getName() != null ? offline.getName() : event.getUniqueId().toString();
-			Bukkit.getConsoleSender().sendMessage( ips.size() + " IPs on record for " + name + ": " + StringUtils.join(ips.toArray(new String[ips.size()]), ", "));
+			Bukkit.getConsoleSender().sendMessage( ips.size() + " IPs on record for " + name
+					+ ": " + StringUtils.join(ips.toArray(new String[ips.size()]), ", "));
 		}
+		if (!allowed) {
+			event.setLoginResult(Result.KICK_OTHER);
+			event.setKickMessage("Your IP address is flagged as unsafe by the Spamhaus XBL."
+					+"\nPlease visit https://www.spamhaus.org/query/ip/" + ip + " to learn why."
+					+ "\n\nIf you have a dynamic IP and this is a recurring problem,"
+					+ "\nplease create a /report at your earliest convenience.");
+			Discord.getInstance().postReport(event.getUniqueId().toString(),
+					ip + " is flagged as unsafe by spamhaus.org/xbl, disconnecting " + event.getUniqueId());
+			return;
+		}
+	}
+
+	private boolean isAllowed(String ip) {
+		if (ip.contains("127.0.0.1")) {
+			return true;
+		}
+		final String[] split = ip.split("\\.");
+		final StringBuilder lookup = new StringBuilder();
+		for (int i = split.length - 1; i >= 0; --i) {
+			lookup.append(split[i]);
+			lookup.append(".");
+		}
+		lookup.append("xbl.spamhaus.org.");
+		try {
+			if (InetAddress.getByName(lookup.toString()) != null) {
+				return false;
+			}
+		} catch (UnknownHostException e) {
+			return true;
+		}
+		return true;
 	}
 }
