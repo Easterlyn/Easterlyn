5598032d32f64b298aa37443e7945e0f617abf71
Jikoo <Jikoo.Games@gmail.com>
2016-11-30 16:52:14 -0500

    Either custom branding is blocked or ProtocolLib isn't fully functional
    I don't really care either way, not married to a silly list change.

--- src/main/java/co/sblock/events/packets/SyncPacketAdapter.java
+++ src/main/java/co/sblock/events/packets/SyncPacketAdapter.java
@@ -4,7 +4,6 @@ package co.sblock.events.packets;
 import java.util.Arrays;
 
 import co.sblock.Sblock;
-import co.sblock.chat.Language;
 import co.sblock.micromodules.DreamTeleport;
 import co.sblock.utilities.PermissionUtils;
 
@@ -12,9 +11,6 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.EnumWrappers.PlayerAction;
-import com.comphenix.protocol.wrappers.WrappedServerPing;
-
-import net.md_5.bungee.api.ChatColor;
 
 /**
  * @author Jikoo
@@ -22,14 +18,11 @@ import net.md_5.bungee.api.ChatColor;
 public class SyncPacketAdapter extends PacketAdapter {
 
 	private final DreamTeleport dream;
-	private final String version;
 
 	public SyncPacketAdapter(Sblock plugin) {
-		super(plugin, PacketType.Status.Server.OUT_SERVER_INFO, PacketType.Play.Client.ENTITY_ACTION,
-				PacketType.Play.Server.TAB_COMPLETE, PacketType.Play.Client.TAB_COMPLETE);
+		super(plugin, PacketType.Play.Client.ENTITY_ACTION, PacketType.Play.Server.TAB_COMPLETE,
+				PacketType.Play.Client.TAB_COMPLETE);
 		this.dream = plugin.getModule(DreamTeleport.class);
-
-		version = plugin.getModule(Language.class).getValue("events.packet.serverList");
 
 		PermissionUtils.addParent("sblock.commands.unfiltered", "sblock.denizen");
 	}
@@ -43,24 +36,7 @@ public class SyncPacketAdapter extends PacketAdapter {
 	 */
 	@Override
 	public void onPacketSending(PacketEvent event) {
-		if (event.getPacketType() == PacketType.Status.Server.OUT_SERVER_INFO) {
-			WrappedServerPing serverping = event.getPacket().getServerPings().read(0);
-
-			// Causes client to see our custom message, cause woo! N.B. Does result in ping breaking
-			// and outdated server displaying.
-			serverping.setVersionProtocol(0);
-
-			int online = serverping.getPlayersOnline(), max = serverping.getPlayersMaximum();
-
-			// Percent-based color: 0-49 = green, 50-74 = yellow, 75-100 = red
-			int percent = online * 100 / max;
-			ChatColor percentColor = percent > 75 ? ChatColor.RED : percent > 50 ? ChatColor.YELLOW
-					: ChatColor.GREEN;
-
-			// Format and away we go
-			serverping.setVersionName(version.replace("{PERCENT}", percentColor.toString())
-					.replace("{ONLINE}", String.valueOf(online)).replace("{MAX}", String.valueOf(max)));
-		} else if (event.getPacketType() == PacketType.Play.Server.TAB_COMPLETE) {
+		if (event.getPacketType() == PacketType.Play.Server.TAB_COMPLETE) {
 			if (event.getPlayer().hasPermission("sblock.commands.unfiltered")) {
 				return;
 			}
