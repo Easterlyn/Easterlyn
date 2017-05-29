7245b60d22e66338df0eaa2b86d25d2d5a83a9f2
Jikoo <Jikoo.Games@gmail.com>
2017-05-29 15:36:45 -0400

    Don't report written book trades, they're free for mail.

--- src/main/java/com/easterlyn/micromodules/VillagerAdjustment.java
+++ src/main/java/com/easterlyn/micromodules/VillagerAdjustment.java
@@ -20,8 +20,8 @@ import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.material.Dye;
 
 /**
- * 
- * 
+ *
+ *
  * @author Jikoo
  */
 public class VillagerAdjustment extends Module {
@@ -33,10 +33,28 @@ public class VillagerAdjustment extends Module {
 		EMERALD_BLOCK(new ItemStack(Material.EMERALD), 729),
 		NOPE(new ItemStack(Material.BARRIER, 65), Integer.MAX_VALUE);
 
+		public static int getValue(final ItemStack currencyItem) {
+			for (CurrencyType currencyType : CurrencyType.values()) {
+				if (currencyType.getCurrencyItem().isSimilar(currencyItem)) {
+					return currencyType.getValue() * currencyItem.getAmount();
+				}
+			}
+			return 0;
+		}
+		public static boolean isCurrency(final ItemStack currencyItem) {
+			for (CurrencyType currencyType : CurrencyType.values()) {
+				if (currencyType.getCurrencyItem().isSimilar(currencyItem)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
 		private final ItemStack currencyItem;
+
 		private final int value;
 
-		private CurrencyType(ItemStack currencyItem, int value) {
+		private CurrencyType(final ItemStack currencyItem, final int value) {
 			this.currencyItem = currencyItem;
 			this.value = value;
 		}
@@ -48,31 +66,13 @@ public class VillagerAdjustment extends Module {
 		public int getValue() {
 			return this.value;
 		}
-
-		public static boolean isCurrency(ItemStack currencyItem) {
-			for (CurrencyType currencyType : values()) {
-				if (currencyType.getCurrencyItem().isSimilar(currencyItem)) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-		public static int getValue(ItemStack currencyItem) {
-			for (CurrencyType currencyType : values()) {
-				if (currencyType.getCurrencyItem().isSimilar(currencyItem)) {
-					return currencyType.getValue() * currencyItem.getAmount();
-				}
-			}
-			return 0;
-		}
 	}
 
 	private final double overpricedRate, normalRate, underpricedRate;
 
 	private Effects effects;
 
-	public VillagerAdjustment(Easterlyn plugin) {
+	public VillagerAdjustment(final Easterlyn plugin) {
 		super(plugin);
 		// TODO: Store somewhere or make externally readable - used by WorthCommand
 		this.overpricedRate = 0.3087;
@@ -80,54 +80,25 @@ public class VillagerAdjustment extends Module {
 		this.underpricedRate = 5.5573;
 	}
 
-	@Override
-	protected void onEnable() {
-		this.effects = this.getPlugin().getModule(Effects.class);
-	}
-
-	@Override
-	protected void onDisable() {}
-
-	public void adjustMerchant(Merchant merchant) {
+	public void adjustMerchant(final Merchant merchant) {
 		List<MerchantRecipe> newRecipes = new ArrayList<>();
 		for (MerchantRecipe recipe : merchant.getRecipes()) {
-			MerchantRecipe newRecipe = adjustRecipe(recipe);
+			MerchantRecipe newRecipe = this.adjustRecipe(recipe);
 			if (newRecipe != null) {
-				newRecipes.add(adjustRecipe(recipe));
+				newRecipes.add(this.adjustRecipe(recipe));
 			}
 		}
 		merchant.setRecipes(newRecipes);
 	}
 
-	public MerchantRecipe adjustRecipe(MerchantRecipe recipe) {
-		if (recipe.getIngredients().size() < 1) {
-			// Ensure recipe has inputs.
-			return null;
-		}
-		ItemStack input1 = recipe.getIngredients().get(0);
-		ItemStack input2 = recipe.getIngredients().size() > 1 ? recipe.getIngredients().get(1) : null;
-		MerchantRecipe adjusted = this.adjustRecipe(input1, input2, recipe.getResult(),
-				recipe.getUses(), recipe.getMaxUses(), recipe.hasExperienceReward());
-
-		if (adjusted == null && !(CurrencyType.isCurrency(input1) && CurrencyType.isCurrency(recipe.getResult()))) {
-			// DEBUG: post report on un-adjustable trades (barring lapis buy trade from priests)
-			this.getPlugin().getModule(Discord.class)
-					.postReport(String.format("Unable to adjust trade: %s + %s -> %s",
-							InventoryUtils.itemToText(input1), InventoryUtils.itemToText(input2),
-							InventoryUtils.itemToText(recipe.getResult())));
-		}
-
-		return adjusted;
-	}
-
 	private MerchantRecipe adjustRecipe(ItemStack input1, ItemStack input2, ItemStack result,
-			int uses, int maxUses, boolean giveExp) {
+			final int uses, final int maxUses, final boolean giveExp) {
 		if (CurrencyType.isCurrency(input1) && (input2 == null || input2.getType() == Material.AIR || CurrencyType.isCurrency(input2))
 				&& !CurrencyType.isCurrency(result)) {
 			// TODO: Does not support value > 64EB (e.g. item worth 80 EB will be unpurchasable instead of 64 and 16 EB)
 			// Purchase result - deal is not supposed to be good.
 			// Use overpriced rate for worth of result.
-			double resultCost = CruxiteDowel.expCost(effects, result) / overpricedRate;
+			double resultCost = CruxiteDowel.expCost(this.effects, result) / this.overpricedRate;
 			// Round down - second stack will cover remainder.
 			input1 = this.getSingleMoneyStack(resultCost, RoundingMode.DOWN);
 
@@ -166,7 +137,7 @@ public class VillagerAdjustment extends Module {
 				&& CurrencyType.isCurrency(result)) {
 			// Sell input - deal is not supposed to be good.
 			// Use overpriced rate for worth of result.
-			double inputCost = CruxiteDowel.expCost(effects, input1) / underpricedRate;
+			double inputCost = CruxiteDowel.expCost(this.effects, input1) / this.underpricedRate;
 			// Round down - reduce value of trade further.
 			result = this.getSingleMoneyStack(inputCost, RoundingMode.DOWN);
 
@@ -189,12 +160,12 @@ public class VillagerAdjustment extends Module {
 				&& !CurrencyType.isCurrency(result)) {
 			// Modification of input for result - deal is not supposed to be good.
 			// Use overpriced rate for worth of result.
-			double resultCost = CruxiteDowel.expCost(effects, result) / overpricedRate;
+			double resultCost = CruxiteDowel.expCost(this.effects, result) / this.overpricedRate;
 			// Use underpriced rate for input.
-			double inputCost = CruxiteDowel.expCost(effects, input1) / underpricedRate;
+			double inputCost = CruxiteDowel.expCost(this.effects, input1) / this.underpricedRate;
 			double cost = Math.abs(resultCost - inputCost);
 			// Round up money.
-			ItemStack money = getSingleMoneyStack(cost, RoundingMode.UP);
+			ItemStack money = this.getSingleMoneyStack(cost, RoundingMode.UP);
 			if (money.getType() == Material.BARRIER) {
 				// Too valuable.
 				return null;
@@ -208,7 +179,37 @@ public class VillagerAdjustment extends Module {
 		return null;
 	}
 
-	private ItemStack getSingleMoneyStack(double worth, RoundingMode mode) {
+	public MerchantRecipe adjustRecipe(final MerchantRecipe recipe) {
+		if (recipe.getIngredients().size() < 1) {
+			// Ensure recipe has inputs.
+			return null;
+		}
+		ItemStack input1 = recipe.getIngredients().get(0);
+		ItemStack input2 = recipe.getIngredients().size() > 1 ? recipe.getIngredients().get(1) : null;
+		MerchantRecipe adjusted = this.adjustRecipe(input1, input2, recipe.getResult(),
+				recipe.getUses(), recipe.getMaxUses(), recipe.hasExperienceReward());
+
+		if (adjusted == null && !(CurrencyType.isCurrency(input1) && CurrencyType.isCurrency(recipe.getResult()))) {
+			if (input1 != null && input1.getType() == Material.WRITTEN_BOOK) {
+				// Skip reporting written book trades.
+				return adjusted;
+			}
+			// DEBUG: post report on un-adjustable trades (barring lapis buy trade from priests)
+			this.getPlugin().getModule(Discord.class)
+					.postReport(String.format("Unable to adjust trade: %s + %s -> %s",
+							InventoryUtils.itemToText(input1), InventoryUtils.itemToText(input2),
+							InventoryUtils.itemToText(recipe.getResult())));
+		}
+
+		return adjusted;
+	}
+
+	@Override
+	public String getName() {
+		return "VillagerAdjustment";
+	}
+
+	private ItemStack getSingleMoneyStack(final double worth, final RoundingMode mode) {
 		CurrencyType[] currencies = CurrencyType.values();
 		CurrencyType currency = currencies[0];
 
@@ -222,7 +223,7 @@ public class VillagerAdjustment extends Module {
 
 		for (int i = currency.ordinal() + 1; amount > 1 && i < currencies.length; ++i) {
 			CurrencyType nextCurrency = currencies[i];
-			double currencyMultiplier = ((double) nextCurrency.getValue()) / currency.getValue();
+			double currencyMultiplier = (double) nextCurrency.getValue() / currency.getValue();
 			if (amount < currencyMultiplier) {
 				break;
 			}
@@ -240,13 +241,13 @@ public class VillagerAdjustment extends Module {
 					|| (amount > currencyMultiplier * 2 &&
 							(mode == RoundingMode.UP || mode == RoundingMode.CEILING)
 							// Rounding up, remainder is close
-							? roundRemainder > currencyMultiplier - (currencyMultiplier / 4)
+							? roundRemainder > currencyMultiplier - currencyMultiplier / 4
 									// Rounding down, remainder is close
-									: (mode == RoundingMode.DOWN || mode == RoundingMode.FLOOR)
+									: mode == RoundingMode.DOWN || mode == RoundingMode.FLOOR
 									? roundRemainder < currencyMultiplier / 4
 											// Other rounding mode, close to either end
-											: Math.abs(roundRemainder - (currencyMultiplier / 2)) < currencyMultiplier / 4)
-					|| amount > currencyMultiplier * 4 && Math.abs(roundRemainder - (currencyMultiplier / 2)) < currencyMultiplier / 6
+											: Math.abs(roundRemainder - currencyMultiplier / 2) < currencyMultiplier / 4)
+					|| amount > currencyMultiplier * 4 && Math.abs(roundRemainder - currencyMultiplier / 2) < currencyMultiplier / 6
 					) {
 				// Set new values
 				currency = nextCurrency;
@@ -276,8 +277,11 @@ public class VillagerAdjustment extends Module {
 	}
 
 	@Override
-	public String getName() {
-		return "VillagerAdjustment";
+	protected void onDisable() {}
+
+	@Override
+	protected void onEnable() {
+		this.effects = this.getPlugin().getModule(Effects.class);
 	}
 
 }
