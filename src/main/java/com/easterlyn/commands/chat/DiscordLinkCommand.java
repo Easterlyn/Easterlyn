5ef74e93fa8417b4c3837a3b54e0515388dbca5e
Adam <jikoo.games@gmail.com>
2017-06-09 21:59:23 -0400

    Sometimes I ask people questions and then actually use their input!

--- src/main/java/com/easterlyn/commands/chat/DiscordLinkCommand.java
+++ src/main/java/com/easterlyn/commands/chat/DiscordLinkCommand.java
@@ -1,29 +1,30 @@
 package com.easterlyn.commands.chat;
 
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-
 import com.easterlyn.Easterlyn;
 import com.easterlyn.commands.EasterlynCommand;
 import com.easterlyn.discord.Discord;
 import com.easterlyn.users.UserRank;
+import com.easterlyn.utilities.JSONUtil;
 import com.easterlyn.utilities.PlayerUtils;
-
 import com.google.common.collect.ImmutableList;
-
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.apache.commons.lang3.StringUtils;
-
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Command for linking a Discord account to Minecraft.
- * 
+ *
  * @author Jikoo
  */
 public class DiscordLinkCommand extends EasterlynCommand {
@@ -81,30 +82,36 @@ public class DiscordLinkCommand extends EasterlynCommand {
 		}
 
 		UUID uuid = ((Player) sender).getUniqueId();
-		Object code;
+		String code;
 		try {
-			code = discord.getAuthCodes().get(uuid);
+			code = discord.getAuthCodes().get(uuid).toString();
 		} catch (ExecutionException e) {
 			// Just re-throw the exception to use our automatic report creation feature
 			throw new RuntimeException(e);
 		}
 
-		if (args.length > 1 && code.toString().equals(args[0])) {
+		if (args.length > 1 && code.equals(args[0])) {
 			// Ensure user can read. Most can't.
 			sender.sendMessage(getLang().getValue("command.link.basicReadingComprehension"));
 			return true;
 		}
 
-		sender.sendMessage(getLang().getValue("command.link.success").replace("{CODE}", code.toString()));
+		BaseComponent component = new TextComponent(JSONUtil.fromLegacyText(
+				getLang().getValue("command.link.success").replace("{CODE}", code)));
+		component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, code));
+		component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+				JSONUtil.fromLegacyText(getLang().getValue("command.link.successHover"))));
+
+		sender.spigot().sendMessage(component);
 		return true;
 	}
 
 	/**
 	 * Check if the Player being linked has a higher permission level than the CommandSender creating the link.
-	 * 
+	 *
 	 * @param player the Player being linked
 	 * @param sender the CommandSender
-	 * 
+	 *
 	 * @return true if the Player being linked has a higher permission level
 	 */
 	private boolean hasHigherPerms(Player player, CommandSender sender) {
