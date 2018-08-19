be10fb1eaba2dd926db9656b71dda41431f2fcb6
Jikoo <Jikoo.Games@gmail.com>
2018-08-19 19:42:36 -0400

    Block nether portals/TPZ outside the world border

---
+++ src/main/java/com/easterlyn/micromodules/protectionhooks/VanillaWorldBorderHook.java
@@ -0,0 +1,54 @@
+package com.easterlyn.micromodules.protectionhooks;
+
+import org.bukkit.Location;
+import org.bukkit.WorldBorder;
+import org.bukkit.entity.Player;
+
+/**
+ * VanillaWorldBorderHook
+ *
+ * @author Jikoo
+ */
+public class VanillaWorldBorderHook extends ProtectionHook {
+
+	VanillaWorldBorderHook() {
+		super("minecraft");
+	}
+
+	@Override
+	public boolean isHookUsable() {
+		return true;
+	}
+
+	@Override
+	public boolean isProtected(Location location) {
+		WorldBorder border = location.getWorld().getWorldBorder();
+		double borderRadius = border.getSize() / 2;
+
+		return border.getCenter().getX() - borderRadius > location.getBlockX()
+				|| border.getCenter().getX() + borderRadius < location.getBlockX()
+				|| border.getCenter().getZ() - borderRadius > location.getBlockZ()
+				|| border.getCenter().getZ() + borderRadius < location.getBlockZ();
+	}
+
+	@Override
+	public boolean canMobsSpawn(Location location) {
+		return !this.isProtected(location);
+	}
+
+	@Override
+	public boolean canUseButtonsAt(Player player, Location location) {
+		return !this.isProtected(location);
+	}
+
+	@Override
+	public boolean canOpenChestsAt(Player player, Location location) {
+		return !this.isProtected(location);
+	}
+
+	@Override
+	public boolean canBuildAt(Player player, Location location) {
+		return !this.isProtected(location);
+	}
+
+}
