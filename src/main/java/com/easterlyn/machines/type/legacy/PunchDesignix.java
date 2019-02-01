77b7f7de3f81c5e5f5e1b115be8728c9af9e55a4
Jikoo <jikoo.games@gmail.com>
2019-02-01 18:53:37 -0500

    Restore Sblock machines

---
+++ src/main/java/com/easterlyn/machines/type/legacy/PunchDesignix.java
@@ -0,0 +1,281 @@
+package com.easterlyn.machines.type.legacy;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.easterlyn.Easterlyn;
+import com.easterlyn.captcha.Captcha;
+import com.easterlyn.machines.MachineInventoryTracker;
+import com.easterlyn.machines.Machines;
+import com.easterlyn.machines.type.Machine;
+import com.easterlyn.machines.utilities.Direction;
+import com.easterlyn.machines.utilities.Shape;
+import com.easterlyn.machines.utilities.Shape.MaterialDataValue;
+import com.easterlyn.utilities.InventoryUtils;
+
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
+
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * Simulate a Sburb Punch Designix in Minecraft.
+ *
+ * @author Jikoo
+ */
+public class PunchDesignix extends Machine {
+
+    /* The ItemStacks used to create usage help trade offers */
+    private static Triple<ItemStack, ItemStack, ItemStack> exampleRecipes;
+
+    private final ItemStack drop;
+    private final Captcha captcha;
+    private final MachineInventoryTracker tracker;
+
+    public PunchDesignix(Easterlyn plugin, Machines machines) {
+        super(plugin, machines, new Shape(), "Punch Designix");
+        this.captcha = plugin.getModule(Captcha.class);
+        tracker = machines.getInventoryTracker();
+        Shape shape = getShape();
+        shape.setVectorData(new Vector(0, 0, 0), shape.new MaterialDataValue(Material.QUARTZ_STAIRS, Direction.WEST, "upperstair"));
+        shape.setVectorData(new Vector(1, 0, 0), shape.new MaterialDataValue(Material.QUARTZ_STAIRS, Direction.EAST, "upperstair"));
+        MaterialDataValue m = shape.new MaterialDataValue(Material.QUARTZ_STAIRS, Direction.NORTH, "upperstair");
+        shape.setVectorData(new Vector(0, 1, 0), m);
+        shape.setVectorData(new Vector(1, 1, 0), m);
+        m = shape.new MaterialDataValue(Material.STEP, (byte) 15);
+        shape.setVectorData(new Vector(0, 0, -1), m);
+        shape.setVectorData(new Vector(1, 0, -1), m);
+        m = shape.new MaterialDataValue(Material.CARPET, (byte) 8);
+        shape.setVectorData(new Vector(0, 1, -1), m);
+        shape.setVectorData(new Vector(1, 1, -1), m);
+
+        drop = new ItemStack(Material.QUARTZ_STAIRS);
+        ItemMeta meta = drop.getItemMeta();
+        meta.setDisplayName(ChatColor.WHITE + "Punch Designix");
+        drop.setItemMeta(meta);
+
+        createExampleRecipes();
+    }
+
+    @Override
+    public boolean handleInteract(PlayerInteractEvent event, ConfigurationSection storage) {
+        if (super.handleInteract(event, storage)) {
+            return true;
+        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return true;
+        }
+        if (event.getPlayer().isSneaking()) {
+            return false;
+        }
+        openInventory(event.getPlayer(), storage);
+        return true;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean handleClick(InventoryClickEvent event, ConfigurationSection storage) {
+        if (event.getSlot() != 2 || event.getRawSlot() != event.getView().convertSlot(event.getRawSlot())) {
+            updateInventory(event.getWhoClicked().getUniqueId(), false);
+            return false;
+        }
+        if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) {
+            updateInventory(event.getWhoClicked().getUniqueId(), false);
+            return true;
+        }
+        // Clicking an item in result slot
+
+        Inventory merchant = event.getInventory();
+
+        // Possible results:
+        // 1) slot 0 is Captcha, slot 1 is null. Result: slot 2 = punch 0. 0, 1 consumed.
+        // 2) slot 0 is Punch, slot 1 is Captcha. Result: slot 2 = copy 0. 1 consumed.
+        // 3) slot 0 is Punch, slot 1 is Punch. Result: slot 2 = combine 0, 1. 0, 1 consumed.
+        ItemStack result = captcha.createCombinedPunch(merchant.getItem(0), merchant.getItem(1));
+
+        if (result == null) {
+            updateInventory(event.getWhoClicked().getUniqueId(), false);
+            return true;
+        }
+
+        int crafts = 1;
+
+        boolean copyPunch = Captcha.isCaptcha(merchant.getItem(1));
+        boolean updateInputSlot0 = false;
+
+        // Clicking a villager result slot with vanilla client treats right clicks as left clicks.
+        if (event.getClick().name().contains("SHIFT")) {
+            // Shift-clicks are craft-max attempts.
+            if (Captcha.isPunch(merchant.getItem(0)) && copyPunch) {
+                crafts = merchant.getItem(1).getAmount();
+            } else {
+                crafts = getMaximumCrafts(merchant.getItem(0), merchant.getItem(1));
+            }
+            result.setAmount(crafts);
+
+            // Decrement number of crafts by number of items that failed to be added
+            // This is only works because the result will always be a single item that stacks to 64
+            crafts -= InventoryUtils.getAddFailures(event.getWhoClicked().getInventory().addItem(result));
+        } else if (event.getCursor() == null || event.getCursor().getType() == Material.AIR
+                || event.getCursor().isSimilar(result)
+                && event.getCursor().getAmount() < event.getCursor().getType().getMaxStackSize()) {
+            // Set cursor to single stack
+            result.setAmount(event.getCursor() == null || event.getCursor().getType() == Material.AIR ? 1 : event.getCursor().getAmount() + 1);
+            event.setCursor(result);
+            updateInputSlot0 = copyPunch;
+        } else {
+            // Invalid craft, cancel and update result
+            updateInventory(event.getWhoClicked().getUniqueId(), false);
+            return true;
+        }
+
+        // This will be recalculated in the synchronous delayed inventory update task.
+        event.setCurrentItem(InventoryUtils.decrement(result, crafts));
+
+        // If second item is a captcha, first item is a punchcard being copied. Do not decrement.
+        if (!copyPunch) {
+            merchant.setItem(0, InventoryUtils.decrement(merchant.getItem(0), crafts));
+        }
+
+        // In all cases (combine, punch single, copy punch) if second is not null it decrements.
+        merchant.setItem(1, InventoryUtils.decrement(merchant.getItem(1), crafts));
+
+        updateInventory(event.getWhoClicked().getUniqueId(), updateInputSlot0);
+        return true;
+    }
+
+    @Override
+    public boolean handleClick(InventoryDragEvent event, ConfigurationSection storage) {
+        updateInventory(event.getWhoClicked().getUniqueId(), false);
+        return false;
+    }
+
+    /**
+     * Calculates the maximum number of items that can be crafted with the given ItemStacks.
+     *
+     * @param slot1 the first ItemStack
+     * @param slot2 the second ItemStack
+     *
+     * @return the least of the two, or, if slot2 is null, the amount in slot1
+     */
+    private int getMaximumCrafts(ItemStack slot1, ItemStack slot2) {
+        return slot2 == null || slot2.getType() == Material.AIR ? slot1.getAmount() : Math.min(slot1.getAmount(), slot2.getAmount());
+    }
+
+    /**
+     * Calculate result slot and update inventory on a delay (post-event completion)
+     *
+     * @param name the name of the player who is using the Punch Designix
+     */
+    public void updateInventory(final UUID id, final boolean updateInputSlot0) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                // Must re-obtain player or update doesn't seem to happen
+                Player player = Bukkit.getPlayer(id);
+                if (player == null || !tracker.hasMachineOpen(player)) {
+                    // Player has logged out or closed inventory. Inventories are per-player, ignore.
+                    return;
+                }
+                if (updateInputSlot0) {
+                    InventoryUtils.updateWindowSlot(player, 0);
+                }
+                Inventory open = player.getOpenInventory().getTopInventory();
+                // TODO this seems to fail to update properly when punch in slot 0 is re-punched
+                ItemStack result = captcha.createCombinedPunch(open.getItem(0), open.getItem(1));
+                open.setItem(2, result);
+                ItemStack inputSlot1 = open.getItem(0);
+                if (inputSlot1 != null) {
+                    inputSlot1 = inputSlot1.clone();
+                    inputSlot1.setAmount(1);
+                }
+                ItemStack inputSlot2 = open.getItem(1);
+                if (inputSlot2 != null) {
+                    inputSlot2 = inputSlot2.clone();
+                    inputSlot2.setAmount(1);
+                }
+                InventoryUtils.updateVillagerTrades(player, getExampleRecipes(),
+                        new ImmutableTriple<>(inputSlot1, inputSlot2, result));
+                InventoryUtils.updateWindowSlot(player, 2);
+            }
+        });
+    }
+
+    /**
+     * Open a PunchDesignix inventory for a Player.
+     *
+     * @param player the Player
+     */
+    public void openInventory(Player player, ConfigurationSection storage) {
+        tracker.openVillagerInventory(player, this, getKey(storage));
+        InventoryUtils.updateVillagerTrades(player, getExampleRecipes());
+    }
+
+    @Override
+    public ItemStack getUniqueDrop() {
+        return drop;
+    }
+
+    /**
+     * Singleton for getting usage help ItemStacks.
+     */
+    public static Triple<ItemStack, ItemStack, ItemStack> getExampleRecipes() {
+        if (exampleRecipes == null) {
+            exampleRecipes = createExampleRecipes();
+        }
+        return exampleRecipes;
+    }
+
+    /**
+     * Creates the ItemStacks used in displaying usage help.
+     *
+     * @return
+     */
+    private static Triple<ItemStack, ItemStack, ItemStack> createExampleRecipes() {
+        ItemStack is1 = new ItemStack(Material.BOOK);
+        ItemMeta im = is1.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Slot 1 options:");
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(ChatColor.GOLD + "1) Captchacard " + ChatColor.DARK_RED + "(consumed)");
+        lore.add(ChatColor.GOLD + "2) Punchcard");
+        lore.add(ChatColor.GOLD + "3) Punchcard " + ChatColor.DARK_RED + "(consumed)");
+        im.setLore(lore);
+        is1.setItemMeta(im);
+
+        ItemStack is2 = new ItemStack(Material.BOOK);
+        im = is2.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Slot 2 options:");
+        lore = new ArrayList<>();
+        lore.add(ChatColor.GOLD + "1) Empty");
+        lore.add(ChatColor.GOLD + "2) Captchacard " + ChatColor.DARK_RED + "(consumed)");
+        lore.add(ChatColor.GOLD + "3) Punchcard " + ChatColor.DARK_RED + "(consumed)");
+        im.setLore(lore);
+        is2.setItemMeta(im);
+
+        ItemStack is3 = new ItemStack(Material.BOOK);
+        im = is3.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Punchcard Result:");
+        lore = new ArrayList<>();
+        lore.add(ChatColor.GOLD + "1) Card 1 punched");
+        lore.add(ChatColor.GOLD + "2) Copy of card 1");
+        lore.add(ChatColor.GOLD + "3) Card 1 and lore of 2");
+        im.setLore(lore);
+        is3.setItemMeta(im);
+
+        return new ImmutableTriple<>(is1, is2, is3);
+    }
+
+}
---
+++ src/main/java/com/easterlyn/machines/type/legacy/TotemLathe.java
@@ -0,0 +1,245 @@
+package com.easterlyn.machines.type.legacy;
+
+import java.util.UUID;
+
+import com.easterlyn.Easterlyn;
+import com.easterlyn.captcha.Captcha;
+import com.easterlyn.captcha.CruxiteDowel;
+import com.easterlyn.machines.MachineInventoryTracker;
+import com.easterlyn.machines.Machines;
+import com.easterlyn.machines.type.Machine;
+import com.easterlyn.machines.utilities.Direction;
+import com.easterlyn.machines.utilities.Shape;
+import com.easterlyn.machines.utilities.Shape.MaterialDataValue;
+import com.easterlyn.utilities.InventoryUtils;
+
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
+
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * Simulates a Totem Lathe from Sburb.
+ *
+ * @author Dublek, Jikoo
+ */
+public class TotemLathe extends Machine	{
+
+    private static Triple<ItemStack, ItemStack, ItemStack> exampleRecipes;
+
+    private final Captcha captcha;
+    private final MachineInventoryTracker tracker;
+    private final ItemStack drop;
+
+    public TotemLathe(Easterlyn plugin, Machines machines) {
+        super(plugin, machines, new Shape(), "Totem Lathe");
+        this.captcha = plugin.getModule(Captcha.class);
+        tracker = machines.getInventoryTracker();
+        Shape shape = getShape();
+        MaterialDataValue m = shape.new MaterialDataValue(Material.QUARTZ_BLOCK, (byte) 2);
+        shape.setVectorData(new Vector(0, 0, 0), m);
+        shape.setVectorData(new Vector(0, 1, 0), m);
+        m = shape.new MaterialDataValue(Material.QUARTZ_BLOCK, (byte) 1);
+        shape.setVectorData(new Vector(0, 2, 0), m);
+        m = shape.new MaterialDataValue(Material.QUARTZ_STAIRS, Direction.WEST, "upperstair");
+        shape.setVectorData(new Vector(1, 0, 0), m);
+        shape.setVectorData(new Vector(1, 2, 0), m);
+        m = shape.new MaterialDataValue(Material.STEP, (byte) 7);
+        shape.setVectorData(new Vector(0, 3, 0), m);
+        shape.setVectorData(new Vector(1, 3, 0), m);
+        shape.setVectorData(new Vector(2, 3, 0), m);
+        m = shape.new MaterialDataValue(Material.STEP, (byte) 15);
+        shape.setVectorData(new Vector(2, 0, 0), m);
+        shape.setVectorData(new Vector(3, 0, 0), m);
+        m = shape.new MaterialDataValue(Material.DAYLIGHT_DETECTOR);
+        shape.setVectorData(new Vector(1, 1, 0), m);
+        m = shape.new MaterialDataValue(Material.ANVIL, Direction.NORTH, "anvil");
+        shape.setVectorData(new Vector(3, 1, 0), m);
+        m = shape.new MaterialDataValue(Material.HOPPER);
+        shape.setVectorData(new Vector(2, 2, 0), m);
+
+        drop = new ItemStack(Material.ANVIL);
+        ItemMeta meta = drop.getItemMeta();
+        meta.setDisplayName(ChatColor.WHITE + "Totem Lathe");
+        drop.setItemMeta(meta);
+    }
+
+    @Override
+    public boolean handleInteract(PlayerInteractEvent event, ConfigurationSection storage) {
+        if (super.handleInteract(event, storage)) {
+            return true;
+        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return true;
+        }
+        if (event.getPlayer().isSneaking()) {
+            return false;
+        }
+        openInventory(event.getPlayer(), storage);
+        return true;
+    }
+
+    /**
+     * Open a Totem Lathe inventory for a Player.
+     *
+     * @param player the Player
+     */
+    public void openInventory(Player player, ConfigurationSection storage) {
+        tracker.openVillagerInventory(player, this, getKey(storage));
+        InventoryUtils.updateVillagerTrades(player, getExampleRecipes());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean handleClick(InventoryClickEvent event, ConfigurationSection storage) {
+        updateInventory(event.getWhoClicked().getUniqueId());
+        if (event.getRawSlot() != event.getView().convertSlot(event.getRawSlot())) {
+            // Clicked inv is not the top.
+            return false;
+        }
+        if (event.getSlot() != 2 || event.getCurrentItem() == null
+                || event.getCurrentItem().getType() == Material.AIR) {
+            // No result
+            return false;
+        }
+        // Item is being crafted
+        Inventory top = event.getView().getTopInventory();
+        Player player = (Player) event.getWhoClicked();
+        int decrement;
+        if (event.getClick().name().contains("SHIFT")) {
+            // This is not a good way to handle shift clicks for normal crafting, but in
+            // this case the result is guaranteed to be a single item that stacks to 64.
+            decrement = Math.min(top.getItem(0).getAmount(), top.getItem(1).getAmount());
+            ItemStack add = event.getCurrentItem().clone();
+            add.setAmount(decrement);
+            if (InventoryUtils.hasSpaceFor(add, player.getInventory())) {
+                player.getInventory().addItem(add);
+            } else {
+                return true;
+            }
+        } else if (event.getCursor() == null
+                || event.getCursor().getType() == Material.AIR
+                || (event.getCursor().isSimilar(event.getCurrentItem())
+                && event.getCursor().getAmount() + event.getCurrentItem().getAmount()
+                <= event.getCursor().getMaxStackSize())) {
+            decrement = 1;
+            ItemStack result = event.getCurrentItem().clone();
+            if (result.isSimilar(event.getCursor())) {
+                result.setAmount(result.getAmount() + event.getCursor().getAmount());
+            }
+            event.setCursor(result);
+        } else {
+            return true;
+        }
+        event.setCurrentItem(null);
+        top.setItem(0, InventoryUtils.decrement(top.getItem(0), decrement));
+        top.setItem(1, InventoryUtils.decrement(top.getItem(1), decrement));
+        player.updateInventory();
+        return true;
+    }
+
+    @Override
+    public boolean handleClick(InventoryDragEvent event, ConfigurationSection storage) {
+        updateInventory(event.getWhoClicked().getUniqueId());
+        return false;
+    }
+
+    /**
+     * Calculate result slot and update inventory on a delay (post-event completion)
+     *
+     * @param name the name of the player who is using the Totem Lathe
+     */
+    public void updateInventory(final UUID id) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                // Must re-obtain player or update doesn't seem to happen
+                Player player = Bukkit.getPlayer(id);
+                if (player == null || !tracker.hasMachineOpen(player)) {
+                    // Player has logged out or closed inventory. Inventories are per-player, ignore.
+                    return;
+                }
+
+                Inventory open = player.getOpenInventory().getTopInventory();
+                ItemStack card = null;
+                ItemStack result = null;
+                ItemStack slot0 = open.getItem(0);
+                ItemStack slot1 = open.getItem(1);
+                if (CruxiteDowel.isBlankDowel(slot0)) {
+                    card = slot1;
+                } else if (CruxiteDowel.isBlankDowel(slot1)) {
+                    card = slot0;
+                }
+                if (Captcha.isPunch(card)) {
+                    result = CruxiteDowel.carve(captcha.captchaToPunch(card));
+                }
+                if (slot0 != null && slot0.getType() != Material.AIR) {
+                    slot0 = slot0.clone();
+                    slot0.setAmount(1);
+                }
+                if (slot1 != null && slot1.getType() != Material.AIR) {
+                    slot1 = slot1.clone();
+                    slot1.setAmount(1);
+                }
+                // Set items
+                open.setItem(2, result);
+                InventoryUtils.updateVillagerTrades(player, getExampleRecipes(),
+                        new ImmutableTriple<>(slot0, slot1, result));
+                InventoryUtils.updateWindowSlot(player, 2);
+            }
+        });
+    }
+
+    @Override
+    public ItemStack getUniqueDrop() {
+        return drop;
+    }
+
+    /**
+     * Singleton for getting usage help ItemStacks.
+     */
+    public static Triple<ItemStack, ItemStack, ItemStack> getExampleRecipes() {
+        if (exampleRecipes == null) {
+            exampleRecipes = createExampleRecipes();
+        }
+        return exampleRecipes;
+    }
+
+    /**
+     * Creates the ItemStacks used in displaying usage help.
+     *
+     * @return
+     */
+    private static Triple<ItemStack, ItemStack, ItemStack> createExampleRecipes() {
+        ItemStack is1 = new ItemStack(Material.NETHER_BRICK_ITEM);
+        ItemMeta im = is1.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Cruxite Totem");
+        is1.setItemMeta(im);
+
+        ItemStack is2 = new ItemStack(Material.BOOK);
+        im = is2.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Punchcard");
+        is2.setItemMeta(im);
+
+        ItemStack is3 = new ItemStack(Material.NETHER_BRICK_ITEM);
+        im = is3.getItemMeta();
+        im.setDisplayName(ChatColor.GOLD + "Carved Totem");
+        is3.setItemMeta(im);
+
+        return new ImmutableTriple<>(is1, is2, is3);
+    }
+
+}
