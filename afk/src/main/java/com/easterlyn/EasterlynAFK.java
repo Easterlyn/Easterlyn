ce98e76e297edff694dd7e6b585db16d07b76c0c
Jikoo <jikoo.games@gmail.com>
2019-07-25 16:38:24 -0400

    Placeholders so I don't forget

---
+++ afk/src/main/java/com/easterlyn/EasterlynAFK.java
@@ -0,0 +1,14 @@
+package com.easterlyn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasterlynAFK extends JavaPlugin {
+
+	/*
+	* TODO:
+	*  - Clear on quit
+	*  - Timer for set: ensure no head movement, not no location movement
+	*  - Reset timer on inventory interact, chat, etc.
+	 */
+
+}
